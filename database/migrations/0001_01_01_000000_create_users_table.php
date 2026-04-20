97f83a47676e918eebc834348f8f7f38e241a17f
Shernil <168435195+Sherkwer@users.noreply.github.com>
2026-04-20 09:50:35 +0800

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,32 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('fname', 150)->nullable();
+            $table->string('mname', 150)->nullable();
+            $table->string('lname', 150)->nullable();
+            $table->string('fullname', 200)->nullable();
+            $table->string('email', 300)->nullable();
+            $table->string('password', 150)->nullable();
+            $table->string('user_role', 60)->nullable();
+            $table->string('area_code', 60)->nullable();
+            $table->string('department_id', 60)->nullable();
+            $table->string('organization_id', 60)->nullable();
+            $table->unsignedBigInteger('area_id')->nullable();
+            $table->string('is_approved', 20)->nullable();
+            $table->integer('is_admin')->nullable();
+            $table->string('remember_token', 100)->nullable();
             $table->timestamps();
+            $table->softDeletes();
+            $table->timestamp('activated_at')->nullable();
+            $table->timestamp('last_login')->nullable();
+            $table->string('is_requested_vrc', 33)->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('verification_code', 50)->nullable();
+            $table->timestamp('verification_created_at')->nullable();
+            $table->timestamp('password_reset_at')->nullable();
+            $table->double('created_user_id')->nullable();
+            $table->double('deleted_user_id')->nullable();
+            $table->double('activated_user_id')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
