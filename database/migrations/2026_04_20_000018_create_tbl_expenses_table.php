f94797a229292c1dd776251f0abaa938bdab53d4
Shernil <168435195+Sherkwer@users.noreply.github.com>
2026-04-21 10:42:19 +0800

    Update 2026_04_20_000018_create_tbl_expenses_table.php

--- database/migrations/2026_04_20_000018_create_tbl_expenses_table.php
+++ database/migrations/2026_04_20_000018_create_tbl_expenses_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->decimal('amount', 10, 0)->nullable();
             $table->string('description', 255)->nullable();
             $table->timestamp('created_at')->useCurrent()->onUpdate(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default('0000-00-00 00:00:00');
+            $table->timestamp('updated_at')->nullable();
             $table->timestamp('deleted_at')->default('0000-00-00 00:00:00');
             $table->timestamp('cancelled_at')->default('0000-00-00 00:00:00');
             $table->integer('created_user_id')->nullable();
