0483b90ce997e3a126e40f7bec322fa44b3c5d3d
Shernil <168435195+Sherkwer@users.noreply.github.com>
2026-04-21 10:43:27 +0800

    Update 2026_04_20_000022_create_tbl_online_payment_table.php

--- database/migrations/2026_04_20_000022_create_tbl_online_payment_table.php
+++ database/migrations/2026_04_20_000022_create_tbl_online_payment_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('reference_id', 255)->nullable();
             $table->string('status', 255)->nullable();
             $table->timestamp('created_at')->useCurrent()->onUpdate(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default('0000-00-00 00:00:00');
+            $table->timestamp('updated_at'->nullable();
             $table->timestamp('deleted_at')->default('0000-00-00 00:00:00');
             $table->timestamp('cancelled_at')->default('0000-00-00 00:00:00');
             $table->integer('created_user_id')->nullable();
